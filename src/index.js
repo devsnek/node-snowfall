3ca031a613d9b2b6498ed937ef00f635e1908ea9
Gus Caplan <me@gus.host>
2017-11-23 11:32:47 -0600

    modernize

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-const Long = require('./vendor/long.min.js');
-
-function pad(v, n, c = '0') {
-  return String(v).length >= n ? String(v) : (String(c).repeat(n) + v).slice(-n);
-}
-
 const COMMON_EPOCHS = {
   UNIX: 0,
   TWITTER: 1288834974657,
@@ -26,22 +20,27 @@ class Snowfall {
   constructor({ workerID, processID, interval, epoch } = {}) {
     this.workerID = workerID || 0;
     this.processID = processID || 0;
-    this.interval = interval || 0;
+    this._interval = interval || 0;
     this.epoch = epoch || COMMON_EPOCHS.UNIX;
+  }
+
+  get interval() {
+    if (this._interval > Number.MAX_SAFE_INT)
+      this._interval = 0;
+    return this._interval++ % 4096;
   }
 
   /**
   * Generate a Snowflake
   * @returns {Snowflake} The generated Snowflake
   */
-  next({ date, interval } = {}) {
-    if (this.interval >= 4096) this.interval = 0;
-    const TIMESTAMP = pad(((date || Date.now()) - this.epoch).toString(2), 42);
-    const WORKER = pad(this.workerID.toString(2), 5);
-    const PROCESS = pad(this.processID.toString(2), 5);
-    const INTERVAL = pad((typeof interval === 'number' ? interval : this.interval++).toString(2), 12);
-    const BINARY = `${TIMESTAMP}${WORKER}${PROCESS}${INTERVAL}`;
-    return Long.fromString(BINARY, 2).toString();
+  next({ date, interval = this.interval } = {}) {
+    const timestamp = ((date || Date.now()) - this.epoch).toString(2).padStart(42, '0');
+    const worker = this.workerID.toString(2).padStart(5, '0');
+    const process = this.processID.toString(2).padStart(5, '0');
+    interval = interval.toString(2).padStart(12, '0');
+    const binary = `${timestamp}${worker}${process}${interval}`;
+    return BigInt.parseInt(binary, 2).toString();
   }
 
   /**
@@ -50,16 +49,17 @@ class Snowfall {
    * @param {number} [epoch=this.epoch] Epoch used to calculate the date
    * @returns {DeconstructedSnowflake} Deconstructed snowflake
    */
-  deconstruct(snowflake, epoch) {
-    epoch = typeof epoch === 'number' ? epoch : this.epoch;
-    if (!epoch) epoch = 0;
-    const BINARY = pad(Long.fromString(snowflake).toString(2), 64);
+  deconstruct(snowflake, epoch = this.epoch) {
+    const binary = BigInt.parseInt(snowflake).toString(2).padStart(64, '0');
     return {
-      date: new Date(parseInt(BINARY.substring(0, 42), 2) + epoch),
-      workerID: parseInt(BINARY.substring(42, 47), 2),
-      processID: parseInt(BINARY.substring(47, 52), 2),
-      interval: parseInt(BINARY.substring(52, 64), 2),
-      binary: BINARY,
+      timestamp: parseInt(binary.substring(0, 42), 2) + epoch,
+      workerID: parseInt(binary.substring(42, 47), 2),
+      processID: parseInt(binary.substring(47, 52), 2),
+      interval: parseInt(binary.substring(52, 64), 2),
+      binary,
+      get date() {
+        return new Date(this.timestamp);
+      },
     };
   }
 
--- test/test.js
+++ test/test.js
@@ -1,26 +1,22 @@
+const assert = require('assert');
 const Snowfall = require('../src');
 
 const flurry = new Snowfall({ epoch: Snowfall.EPOCHS.DISCORD });
-
-function assert(a, b) {
-  if (a !== b) throw new Error(`${a} does not equal ${b}!`);
-  console.log(`${a} is equal to ${b}!`); // eslint-disable-line no-console
-}
 
 let flake = flurry.next();
 
 flake = flurry.next({
   date: new Date('2015-01-01'),
 });
-assert(flake, '1');
+assert.strictEqual(flake, '1');
 
 flake = flurry.next({
   date: new Date('2015-01-01'),
 });
-assert(flake, '2');
+assert.strictEqual(flake, '2');
 
 flake = flurry.next({
   date: new Date('2015-01-01'),
   interval: 0,
 });
-assert(flake, '0');
+assert.strictEqual(flake, '0');
